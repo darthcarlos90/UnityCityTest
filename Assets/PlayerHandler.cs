40f331b6b3ee0d9515e549c2e2b158f3d6532a54
Carlos Tirado <carlos@cratebind.com>
2017-03-15 08:19:02 +1100

    Added a readme.

---
+++ Assets/PlayerHandler.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHandler: MonoBehaviour {
+
+    // Public accesors so the variables can be accesed from the Engine's UI
+    public float speed;
+    public float rotatioSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log("Hello World the time is " + Time.time);
+
+        // Letters must be lowercase
+        
+
+        if (Input.GetKey("w"))
+        {
+            /**
+             * Multiply the value by deltatime so that we have a satic speed no matter the users framerate
+            **/
+            transform.Translate(new Vector3(0, 0, 1) * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey("s"))
+        {
+            transform.Translate(new Vector3(0, 0, -1) * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            transform.Rotate(new Vector3(0, 1, 0) * speed * rotatioSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey("a"))
+        {
+            transform.Rotate(new Vector3(0, -1, 0) * speed * rotatioSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Vehicles/Vehicle Controllers/BaseHelicopterHandler.js
@@ -0,0 +1,79 @@
+﻿#pragma strict
+
+// We use the Editor to assign the two rotor game objects to these variables
+var mainRotor : GameObject;
+var tailRotor : GameObject;
+
+var maxRotorForce : float = 24000;
+var maxRotorVelocity : float =  7200;
+static var rotorVelocity : float = 0;
+private var rotorRotation : float = 0;
+
+var maxTailRotorForce : float = 25000;
+var maxTailRotorVelocity : float = 2200;
+private var tailRotorVelocity : float = 0;
+private var tailRotorRotation : float = 0;
+
+var forwardRotorTorqueMultiplier : float = 0.5;
+var sidewaysRotorTorqueMultiplier : float = 0.5;
+
+var mainRotorActive : boolean = true;
+var tailRotorActive : boolean = true;
+
+private var rb : Rigidbody;
+
+function Start(){
+	rb = GetComponent(Rigidbody);
+}
+
+function FixedUpdate(){
+
+	var torqueValue : Vector3;
+	var controlTorque : Vector3 = new Vector3(Input.GetAxis("Vertical") * forwardRotorTorqueMultiplier, 1.0, -Input.GetAxis("Horizontal2") * sidewaysRotorTorqueMultiplier);
+	if(mainRotorActive == true){
+		torqueValue += (controlTorque * maxRotorForce * rotorVelocity);
+		rb.AddRelativeForce(Vector3.up * maxRotorForce * rotorVelocity);
+	}
+	if(Vector3.Angle(Vector3.up,transform.up) <80 ){
+		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0,transform.rotation.eulerAngles.y,0), Time.deltaTime * rotorVelocity * 2);
+	}
+	if(tailRotorActive == true){
+		torqueValue -= (Vector3.up * maxTailRotorForce * tailRotorVelocity);
+		rb.AddRelativeTorque(torqueValue);
+	}
+}
+
+function Update(){
+	rb = GetComponent(Rigidbody);
+	
+	if(mainRotorActive == true){
+		mainRotor.transform.rotation = transform.rotation * Quaternion.Euler(270,rotorRotation,0);
+	}
+	if(tailRotorActive == true){
+		tailRotor.transform.rotation = transform.rotation * Quaternion.Euler(0,270,tailRotorRotation);
+	}
+	rotorRotation += maxRotorVelocity * rotorVelocity * Time.deltaTime;
+	tailRotorRotation += maxTailRotorVelocity * rotorVelocity * Time.deltaTime;		
+	
+	var hoverRotorVelocity = (rb.mass * Mathf.Abs( Physics.gravity.y ) / maxRotorForce);
+	var hoverTailRotorVelocity = (maxRotorForce * rotorVelocity) / maxTailRotorForce; 
+	
+	if (Input.GetAxis( "Vertical2" ) != 0.0 ) { 
+		rotorVelocity += Input.GetAxis( "Vertical2" ) * 0.005; 
+	}
+	else{
+		rotorVelocity = Mathf.Lerp( rotorVelocity, hoverRotorVelocity, Time.deltaTime * Time.deltaTime * 5 ); 
+	} 
+	tailRotorVelocity = hoverTailRotorVelocity - Input.GetAxis( "Horizontal" ) * 0.01; 
+	if ( rotorVelocity > 1.0 ) { 
+		rotorVelocity = 1.0; 
+	}
+	else if ( rotorVelocity < 0.0 ) { 
+		rotorVelocity = 0.0; 
+	}
+}
+
+
+
+
+	
