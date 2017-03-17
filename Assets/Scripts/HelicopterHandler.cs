c9395a7b36fddfc0404a5950e55a59321a76d487
Carlos Tirado <carlos@cratebind.com>
2017-03-17 16:18:33 +1100

    Added some elements to the scripts that will help to run the helicopter.

--- Assets/Scripts/HelicopterHandler.cs
+++ Assets/Scripts/HelicopterHandler.cs
@@ -2,32 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class HelicopterHandler: MonoBehaviour {
 
+    // These game objects are used to represent the main and tal rotors on the helicopter game object.
+    public GameObject mainRotor;
+    public GameObject tailRotor;
 
-    GameObject mainRotor;
-    GameObject tailRotor;
-
-    float maxRotorForce = 24000;
-    float maxRotorVelocity = 7200;
+    public float maxRotorForce = 24000;
+    public float maxRotorVelocity = 7200;
 
     static float rotorVelocity = 0;
     private float rotorRotation = 0;
 
-    float maxTailRotorForce = 25000;
-    float maxTailRotorVelocity = 22000;
+    public float maxTailRotorForce = 25000;
+    public float maxTailRotorVelocity = 22000;
 
     private float tailRotorVelocity = 0;
     private float tailRotorRotation = 0;
 
-    float forwardRotorTorqueMultiplier = 0.5f;
-    float sidewayRotorTorqueMultiplier = 0.5f;
+    public float forwardRotorTorqueMultiplier = 0.5f;
+    public float sidewayRotorTorqueMultiplier = 0.5f;
 
-    bool mainRotorActive = true;
-    bool tailRotorActive = true;
+    public bool mainRotorActive = true;
+    public bool tailRotorActive = true;
 
-    private Rigidbody rb; 
+    private Rigidbody rb;
 
+
+    private void Start()
+    {
+        // Since we will attach this script to a game object, using the get component function will get
+        // the rigid body that this attached to this element
+        // TODO Check if this is null, if so it is not working
+        Debug.Log("Start function");
+        rb = GetComponent<Rigidbody>();
+
+        if(rb == null)
+        {
+            Debug.Log("This element is null");
+        }
+    }
 
 
 
